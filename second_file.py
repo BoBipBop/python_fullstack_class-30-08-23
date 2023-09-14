9c5224a93f88fcb0928bf87ea3b44871e3d18b8f
Sergey Barkar <ipbarkar@yandex.ru>
2023-09-14 11:29:42 +0300

    Add second_file and third_file

---
+++ second_file.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 10:47:47 2023
+
+@author: Mi
+"""
+# Тут будет второй код на Python
---
+++ third_file.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 11:27:47 2023
+
+@author: Mi
+"""
+
+# Тут будет третий код на Python
